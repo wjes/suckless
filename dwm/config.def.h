bcffa50db98822d92b87f39b85d449e3dd8164ce
Warren Eaton <warren.eaton.sciaccaluga@gmail.com>
2022-11-07 09:04:19 -0300

    Delete on-hove window focus

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -6,6 +6,7 @@ static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
+static const int focusonwheel       = 0;
 static const char *fonts[]          = { "monospace:size=10" };
 static const char dmenufont[]       = "monospace:size=10";
 
@@ -37,7 +38,7 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "", "", "", "", "", "", "" };
+static const char *tags[] = { "", "", "", "", "", "", "" };
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -57,9 +58,9 @@ static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen win
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
-    { "T",      tile },    /* first entry is default */
-    { "F",      NULL },    /* no layout function means floating behavior */
-    { "M",      monocle },
+    { "﩯",      tile },    /* first entry is default */
+    { "ﱢ",      NULL },    /* no layout function means floating behavior */
+    { "",      monocle },
 };
 
 /* key definitions */
@@ -76,7 +77,8 @@ static const Layout layouts[] = {
 /* commands */
 static const char *dmenucmd[] = { "dmenu_run", "-fn", dmenufont, "-nb", nord0, "-nf", nord4, "-sb", nord3, "-sf", nord5, NULL };
 static const char *termcmd[]  = { "st", NULL };
-static const char *mutecmd[] = { "pactl", "set-sink-mute", "0", "toggle", NULL };
+static const char *mutevolcmd[] = { "pactl", "set-sink-mute", "0", "toggle", NULL };
+static const char *mutemiccmd[] = { "pactl", "set-source-mute", "@DEFAULT_SOURCE@", "toggle", NULL };
 static const char *volupcmd[] = { "pactl", "set-sink-volume", "0", "+5%", NULL };
 static const char *voldowncmd[] = { "pactl", "set-sink-volume", "0", "-5%", NULL };
 static const char *brupcmd[] = { "sudo", "light", "-A", "10", NULL };
@@ -117,11 +119,12 @@ static const Key keys[] = {
     TAGKEYS(            XK_8,                                       7)
     TAGKEYS(            XK_9,                                       8)
     { MODKEY|Mod1Mask,  XK_Delete,                  quit,           {0} },
-    { 0,                XF86XK_AudioMute,           spawn,          {.v = mutecmd } },
+    { 0,                XF86XK_AudioMute,           spawn,          {.v = mutevolcmd } },
+    { 0,                XF86XK_AudioMicMute,        spawn,          {.v = mutemiccmd } },
     { 0,                XF86XK_AudioLowerVolume,    spawn,          {.v = voldowncmd } },
     { 0,                XF86XK_AudioRaiseVolume,    spawn,          {.v = volupcmd } },
     { 0,                XF86XK_MonBrightnessUp,     spawn,          {.v = brupcmd} },
-    { 0,                XF86XK_MonBrightnessDown,   spawn,          {.v = brdowncmd} },
+    { 0,                XF86XK_MonBrightnessDown,   spawn,          {.v = brdowncmd } },
 };
 
 /* button definitions */
--- dwm/dwm.c
+++ dwm/dwm.c
@@ -163,7 +163,7 @@ static void detachstack(Client *c);
 static Monitor *dirtomon(int dir);
 static void drawbar(Monitor *m);
 static void drawbars(void);
-static void enternotify(XEvent *e);
+// static void enternotify(XEvent *e);
 static void expose(XEvent *e);
 static void focus(Client *c);
 static void focusin(XEvent *e);
@@ -182,7 +182,7 @@ static void manage(Window w, XWindowAttributes *wa);
 static void mappingnotify(XEvent *e);
 static void maprequest(XEvent *e);
 static void monocle(Monitor *m);
-static void motionnotify(XEvent *e);
+// static void motionnotify(XEvent *e);
 static void movemouse(const Arg *arg);
 static Client *nexttiled(Client *c);
 static void pop(Client *c);
@@ -250,13 +250,13 @@ static void (*handler[LASTEvent]) (XEvent *) = {
 	[ConfigureRequest] = configurerequest,
 	[ConfigureNotify] = configurenotify,
 	[DestroyNotify] = destroynotify,
-	[EnterNotify] = enternotify,
+	// [EnterNotify] = enternotify,
 	[Expose] = expose,
 	[FocusIn] = focusin,
 	[KeyPress] = keypress,
 	[MappingNotify] = mappingnotify,
 	[MapRequest] = maprequest,
-	[MotionNotify] = motionnotify,
+	// [MotionNotify] = motionnotify,
 	[PropertyNotify] = propertynotify,
 	[UnmapNotify] = unmapnotify
 };
@@ -427,7 +427,9 @@ buttonpress(XEvent *e)
 
 	click = ClkRootWin;
 	/* focus monitor if necessary */
-	if ((m = wintomon(ev->window)) && m != selmon) {
+	// if ((m = wintomon(ev->window)) && m != selmon) {
+    if ((m = wintomon(ev->window)) && m != selmon
+         && (focusonwheel || (ev->button != Button4 && ev->button != Button5))) {
 		unfocus(selmon->sel, 1);
 		selmon = m;
 		focus(NULL);
@@ -447,8 +449,10 @@ buttonpress(XEvent *e)
 		else
 			click = ClkWinTitle;
 	} else if ((c = wintoclient(ev->window))) {
-		focus(c);
-		restack(selmon);
+		// focus(c);
+		// restack(selmon);
+        if (focusonwheel || (ev->button != Button4 && ev->button != Button5))
+            focus(c);
 		XAllowEvents(dpy, ReplayPointer, CurrentTime);
 		click = ClkClientWin;
 	}
@@ -731,12 +735,20 @@ drawbar(Monitor *m)
 				urg & 1 << i);
 		x += w;
 	}
-	w = TEXTW(m->ltsymbol);
+
 	drw_setscheme(drw, scheme[SchemeNorm]);
+
+	w = TEXTW("|");
+	x = drw_text(drw, x, 0, w, bh, lrpad / 2, "|", 0);
+
+	w = TEXTW(m->ltsymbol);
 	x = drw_text(drw, x, 0, w, bh, lrpad / 2, m->ltsymbol, 0);
 
+	w = TEXTW("|");
+	x = drw_text(drw, x, 0, w, bh, lrpad / 2, "|", 0);
+
 	if ((w = m->ww - tw - x) > bh) {
-		drw_setscheme(drw, scheme[SchemeNorm]);
+		// drw_setscheme(drw, scheme[SchemeNorm]);
 		if (m->sel) {
 			drw_text(drw, x, 0, w, bh, lrpad / 2, m->sel->name, 0);
 			if (m->sel->isfloating)
@@ -757,24 +769,24 @@ drawbars(void)
 		drawbar(m);
 }
 
-void
-enternotify(XEvent *e)
-{
-	Client *c;
-	Monitor *m;
-	XCrossingEvent *ev = &e->xcrossing;
-
-	if ((ev->mode != NotifyNormal || ev->detail == NotifyInferior) && ev->window != root)
-		return;
-	c = wintoclient(ev->window);
-	m = c ? c->mon : wintomon(ev->window);
-	if (m != selmon) {
-		unfocus(selmon->sel, 1);
-		selmon = m;
-	} else if (!c || c == selmon->sel)
-		return;
-	focus(c);
-}
+// void
+// enternotify(XEvent *e)
+// {
+// 	Client *c;
+// 	Monitor *m;
+// 	XCrossingEvent *ev = &e->xcrossing;
+//
+// 	if ((ev->mode != NotifyNormal || ev->detail == NotifyInferior) && ev->window != root)
+// 		return;
+// 	c = wintoclient(ev->window);
+// 	m = c ? c->mon : wintomon(ev->window);
+// 	if (m != selmon) {
+// 		unfocus(selmon->sel, 1);
+// 		selmon = m;
+// 	} else if (!c || c == selmon->sel)
+// 		return;
+// 	focus(c);
+// }
 
 void
 expose(XEvent *e)
@@ -1110,27 +1122,27 @@ monocle(Monitor *m)
 		if (ISVISIBLE(c))
 			n++;
 	if (n > 0) /* override layout symbol */
-		snprintf(m->ltsymbol, sizeof m->ltsymbol, "[%d]", n);
+		snprintf(m->ltsymbol, sizeof m->ltsymbol, " [%d]", n);
 	for (c = nexttiled(m->clients); c; c = nexttiled(c->next))
 		resize(c, m->wx, m->wy, m->ww - 2 * c->bw, m->wh - 2 * c->bw, 0);
 }
 
-void
-motionnotify(XEvent *e)
-{
-	static Monitor *mon = NULL;
-	Monitor *m;
-	XMotionEvent *ev = &e->xmotion;
-
-	if (ev->window != root)
-		return;
-	if ((m = recttomon(ev->x_root, ev->y_root, 1, 1)) != mon && mon) {
-		unfocus(selmon->sel, 1);
-		selmon = m;
-		focus(NULL);
-	}
-	mon = m;
-}
+// void
+// motionnotify(XEvent *e)
+// {
+// 	static Monitor *mon = NULL;
+// 	Monitor *m;
+// 	XMotionEvent *ev = &e->xmotion;
+//
+// 	if (ev->window != root)
+// 		return;
+// 	if ((m = recttomon(ev->x_root, ev->y_root, 1, 1)) != mon && mon) {
+// 		unfocus(selmon->sel, 1);
+// 		selmon = m;
+// 		focus(NULL);
+// 	}
+// 	mon = m;
+// }
 
 void
 movemouse(const Arg *arg)
